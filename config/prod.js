db34d9f49649af6338abfd45a02af58a8500a993
christos.dimizas <christos.dimizas@marinetraffic.com>
2018-02-20 15:42:16 +0200

    production vs dev setup and deployment

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI, // 'mongodb://dimizasProd:2710544601@ds143388.mlab.com:43388/emaily-db-prod',
+    cookieKey: process.env.COOKIE_KEY, // 'fwrfwrwrbrntukiulliiokuty64tbtjk8j',
+    googleClientID: process.env.GOOGLE_CLIENT_ID, // '385743603471-fh6g6it90i7kuvnf3osjnk3h70m47ojf.apps.googleusercontent.com',
+    googleClientSecretKey: process.env.GOOGLE_CLIENT_SECRET, // '-s-GO-eOAjcf1OmdKheQZlx9',
+};
