0fb45ca882553aeb902b851ae1b6b6f58a83954a
christos.dimizas <christos.dimizas@marinetraffic.com>
2018-02-22 14:53:13 +0200

    Create landing draft component. Change anchor link on header with a Link component which redirected user with respect of the user log status

---
+++ client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Landing extends React.Component {
+
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1>
+                    Emaily
+                </h1>
+                Collect feedback from your users
+            </div>
+        )
+    }
+}
+
+export default Landing;
