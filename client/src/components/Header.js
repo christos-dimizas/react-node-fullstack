362b4f9caaa609821bd9bc3d367150bda6e345c1
christos.dimizas <christos.dimizas@marinetraffic.com>
2018-02-22 12:23:33 +0200

    Install and start adding materilize css into projects header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,14 @@ export default class Header extends React.Component {
 
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">Emaily</a>
+                    <ul className="right">
+                        <li><a>Login wih Google</a></li>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/App';
 import reducers from './reducers'
 
+import 'materialize-css/dist/css/materialize.min.css';
 
 const store = createStore(reducers, {}, applyMiddleware());
 ReactDOM.render(
