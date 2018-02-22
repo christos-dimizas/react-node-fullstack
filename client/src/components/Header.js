c703a1d9ffac2f0ec38ae3644c8ebb497493aac1
christos.dimizas <christos.dimizas@marinetraffic.com>
2018-02-22 14:29:40 +0200

    Configure header for current user status display handling, redirect user to dashboard page after login

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-export default class Header extends React.Component {
+class Header extends React.Component {
+
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google">Login with Google</a>
+                    </li>
+                );
+            default:
+                return (
+                    <li>
+                        <a href="/api/logout">Logout</a>
+                    </li>
+                );
+
+        }
+    }
 
     render() {
+        console.log(this.props);
         return (
             <nav>
                 <div className="nav-wrapper">
                     <a className="left brand-logo">Emaily</a>
                     <ul className="right">
-                        <li><a>Login wih Google</a></li>
+                        <li>
+                            {this.renderContent()}
+                        </li>
                     </ul>
                 </div>
             </nav>
         )
     }
 }
+
+function mapStateToProps({auth}) {
+    return {auth}
+}
+
+export default connect(mapStateToProps)(Header);
