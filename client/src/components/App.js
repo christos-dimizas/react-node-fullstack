a22df087c3ae3b0ff14607aa0e6282e0c0c8fb62
christos.dimizas <christos.dimizas@marinetraffic.com>
2018-02-22 12:03:53 +0200

    Initialize routing with dummy components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+import Header from './Header';
+const Dashboard = () => (<h2>Dashboard</h2>);
+const SurveyNew = () => (<h2>SurveyNew</h2>);
+const Landing = () => (<h2>Landing</h2>);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-          <a href="/auth/google">Google login</a>
+          <BrowserRouter>
+              <div>
+                  <Header />
+                  <Route exact path="/" component={Landing} />
+                  <Route exact path="/surveys" component={Dashboard} />
+                  <Route path="/surveys/new" component={SurveyNew} />
+              </div>
+          </BrowserRouter>
       </div>
     );
   }
