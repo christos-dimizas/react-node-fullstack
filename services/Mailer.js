2465c70f9ac4a275559952f92519b378e3499d4b
Christos Dimizas <christos.dimizas@marinetraffic.com>
2018-03-02 17:40:55 +0200

    Continue with Mailer class setup

--- services/Mailer.js
+++ services/Mailer.js
@@ -11,6 +11,30 @@ class Mailer extends helper.Mail {
         this.subject = subject;
         this.body = new helper.Content('text/html', mailContent);
         this.recipients = this.formatAddresses(recipients);
+
+        this.addContent(this.body); // addContent comes from the Mail extension
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddresses(recipients) {
+        return recipients.map(({email}) => {
+            return new helper.Email(email);
+        });
+    }
+
+    addClickTracking() {
+        // The below code comes from the SendBox documentation
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    // TODO - TO BE CONTINUED
+    addRecipients() {
+
     }
 }
 
